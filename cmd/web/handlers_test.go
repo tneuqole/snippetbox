98ce5ce505cced049b98da883d037c2cbdd52057
tneuqole <woodmanjd32@gmail.com>
2024-11-06 12:49:53 -0500

    test snippet create

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -183,3 +183,33 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, header, _ := ts.get(t, "/snippet/create")
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, "/user/login", header.Get("Location"))
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "user@example.com")
+		form.Add("password", "password")
+		form.Add("csrf_token", csrfToken)
+		code, header, _ := ts.postForm(t, "/user/login", form)
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, "/", header.Get("Location"))
+
+		code, _, body = ts.get(t, "/snippet/create")
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, `<form action="/snippet/create" method="POST">`)
+	})
+}
